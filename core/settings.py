1f5a894989a4e2f89bceda28c86ee076d687764a
serpel <serpel.js@gmail.com>
2020-09-04 03:21:14 -0600

    added heroku deployment packages

--- core/settings.py
+++ core/settings.py
@@ -8,6 +8,7 @@ import os
 from decouple import config
 from unipath import Path
 import dj_database_url
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR    = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -128,3 +129,6 @@ STATICFILES_DIRS = (
 )
 #############################################################
 #############################################################
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
